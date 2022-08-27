91438e145f91f92f627aca853710082e898d2dcb
이기문 <kimoon212@gmail.com>
2022-08-27 23:06:36 +0900

    firebase 데이터 받아오기

---
+++ src/components/MemberLookup.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
+function MemberLookup() {
+  //
+  return (
+    <AsideContainer>
+      <h3>내전 참여 현황</h3>
+    </AsideContainer>
+  );
+}
+
+export default MemberLookup;
+
+const AsideContainer = styled.aside`
+  width: 200px;
+  height: 2000px;
+  border: 1px solid black;
+`;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../app/firebase';
+import MemberLookup from '../components/MemberLookup';
 
 function Home() {
+  const [Infos, setInfos] = useState();
+
+  const querySnap = async () => {
+    const data = await getDocs(collection(db, 'gameInfos'));
+    let temp = [];
+    data.forEach((doc) => {
+      // console.log(doc.data());
+      temp.push(doc.data());
+    });
+    setInfos([...temp]);
+    return temp;
+  };
+
+  useEffect(() => {
+    querySnap();
+    console.log(Infos);
+  }, []);
+  const InfosHandler = (data) => {
+    setInfos([...Infos, data]);
+  };
+
+  console.log(Infos);
   return (
-    <div>
+    <>
       <h2>8월 내전 통계</h2>
-      <aside>내전참여 인원</aside>
-      <div>
-        <div>8월 mvp 1등</div>
-        <div>8월 연패 1등</div>
-        <div>내전 승률 왕</div>
-        <div>각 분야 1등</div>
-        <div>내전 벤픽 현황</div>
-      </div>
-      <Link to='/setmember'>이동하기</Link>
-    </div>
+      <HomeContainer>
+        <React.Suspense fallback='...Loading'>
+          <MemberLookup />
+        </React.Suspense>
+        ;
+        <div>
+          <div>8월 mvp 1등</div>
+          <div>8월 연패 1등</div>
+          <div>내전 승률 왕</div>
+          <div>각 분야 1등</div>
+          <div>내전 벤픽 현황</div>
+        </div>
+        <Link to='/setmember'>이동하기</Link>
+      </HomeContainer>
+    </>
   );
 }
 
 export default Home;
+
+const HomeContainer = styled.main`
+  width: 1200px;
+
+  display: flex;
+`;
