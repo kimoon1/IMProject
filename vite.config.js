fccd74a3248d01b963e14799524039f730ee3124
이기문 <kimoon212@gmail.com>
2022-09-05 17:50:28 +0900

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: 'codeclan.github.io',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
 });
